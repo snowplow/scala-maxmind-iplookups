c6b20b42dfd2e9860a493ca43f9aa68cdf36cb1c
Alex Dean <alexander.dean@keplarllp.com>
2013-01-01 20:17:17 +0200

    Stubbed files

---
+++ project/Dependencies.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2012 SnowPlow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+import sbt._
+
+object Dependencies {
+	val resolutionRepos = Seq(
+		ScalaToolsSnapshots,
+		"Concurrent Maven Repo" at "http://conjars.org/repo", // For Scalding, Cascading etc
+		"Twitter Maven Repo" at "http://maven.twttr.com/" // For Twitter's "wonderful" util functions
+	)
+
+	object Urls {
+		val maxmindJava = "http://www.maxmind.com/download/geoip/api/java/GeoIPJava-%s.zip"
+		val maxmindData = "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz"
+	}
+
+	object V {
+		val maxmind     = "1.2.8" // Compiled in BuildSettings
+		val scalding    = "0.7.3"
+		val collUtils   = "5.3.10"
+		val specs2      = "1.8"
+	}
+
+	object Libraries {
+		val scalding    = "com.twitter"                %% "scalding"            % V.scalding
+		val collUtils	  = "com.twitter"								 %  "util-collection"		  % V.collUtils
+		val specs2      = "org.specs2"                 %% "specs2"              % V.specs2       % "test"
+	}
+}
---
+++ src/main/scala/com/snowplowanalytics/hadoop/etl/EtlRunner.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012 SnowPlow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.hadoop.etl
+
+// Hadoop
+import org.apache.hadoop.util.ToolRunner
+import org.apache.hadoop.conf.Configuration
+
+// Scalding
+import com.twitter.scalding.Tool
+
+/**
+ * Entrypoint for Hadoop to kick off the ETL job.
+ *
+ * Borrowed from com.twitter.scalding.Tool
+ */
+object EtlRunner {
+	def main(args : Array[String]) {
+		ToolRunner.run(new Configuration, new Tool, args);
+	}
+}

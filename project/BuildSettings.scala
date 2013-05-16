a3f8ce670b20ec2d0fcc733e34720165939a44ba
Alex Dean <alexander.dean@keplarllp.com>
2013-05-16 20:40:05 +0100

    Bumped version

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -20,7 +20,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization  := "com.snowplowanalytics",
-    version       := "0.0.3",
+    version       := "0.0.4",
     description   := "Scala wrapper for MaxMind GeoIP library",
     scalaVersion  := "2.9.1",
     scalacOptions := Seq("-deprecation", "-encoding", "utf8"),

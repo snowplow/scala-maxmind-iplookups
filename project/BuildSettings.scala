2f5cd147912a5ce360058e832b35a4431dd7aaf4
Fred Blundun <fred@snowplowanalytics.com>
2014-07-03 16:21:20 +0100

    Decreased Scala version to 2.9.3 and updated .travis.yml with new versions to test

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -22,8 +22,8 @@ object BuildSettings {
     organization  := "com.snowplowanalytics",
     version       := "0.1.0",
     description   := "Scala wrapper for MaxMind GeoIP library",
-    scalaVersion  := "2.10.1",
-    crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10.0", "2.10.1"),
+    scalaVersion  := "2.9.3",
+    crossScalaVersions := Seq("2.9.3", "2.10.0", "2.10.1", "2.10.4"),
     scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
     resolvers     ++= Dependencies.resolutionRepos
   )

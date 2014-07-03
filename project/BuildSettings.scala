6c22d2b495de35d8a9cb2f253498500fe9d4639c
Fred Blundun <fred@snowplowanalytics.com>
2014-07-03 16:23:49 +0100

    Dropped Scala version 2.10.1 from BuildSettings.scala

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -23,7 +23,7 @@ object BuildSettings {
     version       := "0.1.0",
     description   := "Scala wrapper for MaxMind GeoIP library",
     scalaVersion  := "2.9.3",
-    crossScalaVersions := Seq("2.9.3", "2.10.0", "2.10.1", "2.10.4"),
+    crossScalaVersions := Seq("2.9.3", "2.10.0", "2.10.4"),
     scalacOptions := Seq("-deprecation", "-encoding", "utf8"),
     resolvers     ++= Dependencies.resolutionRepos
   )

a84b36f9a104c72eb8139136c6b7047eeba8189d
Fred Blundun <fred@snowplowanalytics.com>
2014-09-15 15:00:37 +0100

    Bumped version to 0.2.0

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -20,7 +20,7 @@ object BuildSettings {
   // Basic settings for our app
   lazy val basicSettings = Seq[Setting[_]](
     organization  := "com.snowplowanalytics",
-    version       := "0.1.0",
+    version       := "0.2.0",
     description   := "Scala wrapper for MaxMind GeoIP library",
     scalaVersion  := "2.9.3",
     crossScalaVersions := Seq("2.9.3", "2.10.4"),

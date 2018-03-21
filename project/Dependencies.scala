515cc65ccf0c886e176ed55a9bbe1260e16823bf
Ben Fradet <benjamin.fradet@gmail.com>
2018-03-21 13:34:48 +0000

    Bump specs2 to 4.0.3 (closes #49)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,5 +20,5 @@ object Dependencies {
   val maxmind   = "com.maxmind.geoip2" %  "geoip2"          % "2.11.0"
   val collUtils = "com.twitter"        %% "util-collection" % "18.2.0"
   val scalaz    = "org.scalaz"         %% "scalaz-core"     % "7.0.9"
-  val specs2    = "org.specs2"         %% "specs2"          % "2.3.13" % "test"
+  val specs2    = "org.specs2"         %% "specs2-core"     % "4.0.3"  % "test"
 }

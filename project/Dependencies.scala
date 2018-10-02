a76eb1001f2d6f220c7a41ee19d7eff2e58c2e02
Mukund Ananthu <MukundAnanthu@users.noreply.github.com>
2018-10-02 15:07:35 +0530

    Bump geoip2 to 2.12.0 (closes #93)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@
 import sbt._
 
 object Dependencies {
-  val maxmind       = "com.maxmind.geoip2"    %  "geoip2"               % "2.11.0"
+  val maxmind       = "com.maxmind.geoip2"    %  "geoip2"               % "2.12.0"
   val catsEffect    = "org.typelevel"         %% "cats-effect"          % "0.10.1"
   val cats          = "org.typelevel"         %% "cats-core"            % "1.1.0"
   val lruMap        = "com.snowplowanalytics" %% "scala-lru-map"        % "0.1.0"

a6d8cda82a3fa12626f90bed6ca979079d475a5b
Ben Fradet <benjamin.fradet@gmail.com>
2019-03-06 14:11:36 +0100

    Introduce tagless final encoding (closes #108)

---
+++ src/main/scala/com.snowplowanalytics.maxmind.iplookups/IpAddressResolver.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2012-2018 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.maxmind.iplookups
+
+import java.net.InetAddress
+
+import cats.Eval
+import cats.effect.Sync
+import cats.syntax.either._
+
+/** Data type letting you resolve IP address */
+sealed trait IpAddressResolver[F[_]] {
+  def resolve(ip: String): F[Either[Throwable, InetAddress]]
+
+  protected def getIpAddress(ip: String): Either[Throwable, InetAddress] =
+    Either.catchNonFatal(InetAddress.getByName(ip))
+}
+
+object IpAddressResolver {
+  implicit def ipAddressResolver[F[_]: Sync]: IpAddressResolver[F] = new IpAddressResolver[F] {
+    def resolve(ip: String): F[Either[Throwable, InetAddress]] =
+      Sync[F].delay { getIpAddress(ip) }
+  }
+
+  implicit def evalIpAddressResolver: IpAddressResolver[Eval] = new IpAddressResolver[Eval] {
+    def resolve(ip: String): Eval[Either[Throwable, InetAddress]] =
+      Eval.later { getIpAddress(ip) }
+  }
+}

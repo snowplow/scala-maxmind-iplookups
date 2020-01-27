be34040c5e11de109fd092b2374563a122ecd8fb
Marek Żebrowski <marek.zebrowski@gmail.com>
2020-01-27 12:52:06 +0100

    down cats-core

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ import sbt._
 object Dependencies {
   val maxmind       = "com.maxmind.geoip2"    %  "geoip2"               % "2.12.0"
   val catsEffect    = "org.typelevel"         %% "cats-effect"          % "2.0.0"
-  val cats          = "org.typelevel"         %% "cats-core"            % "2.1.0"
+  val cats          = "org.typelevel"         %% "cats-core"            % "2.0.0"
   val lruMap        = "com.snowplowanalytics" %% "scala-lru-map"        % "0.4.0-SNAPSHOT"
   val scalaCheck    = "org.scalacheck"        %% "scalacheck"           % "1.14.0" % Test
   val specs2        = "org.specs2"            %% "specs2-core"          % "4.8.3"  % Test

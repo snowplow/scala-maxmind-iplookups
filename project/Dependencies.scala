4e2fe58291654b4020843dbbc73bee78c57fff11
Ian Streeter <ian@snowplowanalytics.com>
2021-08-09 21:53:31 +0100

    Bump cats-effect to 2.5.3 (close #156)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ import sbt._
 
 object Dependencies {
   val maxmind       = "com.maxmind.geoip2"    %  "geoip2"               % "2.15.0"
-  val catsEffect    = "org.typelevel"         %% "cats-effect"          % "2.1.3"
+  val catsEffect    = "org.typelevel"         %% "cats-effect"          % "2.5.3"
   val cats          = "org.typelevel"         %% "cats-core"            % "2.1.1"
   val lruMap        = "com.snowplowanalytics" %% "scala-lru-map"        % "0.4.0"
   val scalaCheck    = "org.scalacheck"        %% "scalacheck"           % "1.14.3" % Test
